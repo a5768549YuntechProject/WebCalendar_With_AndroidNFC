ee33f0979f5a558653a1f46c48a017e0918f216c
a5768549 <44581221+a5768549@users.noreply.github.com>
2021-04-22 15:10:55 +0800

    add some click event

--- express_nfc/app/src/main/java/com/example/express_nfc/MainActivity.java
+++ express_nfc/app/src/main/java/com/example/express_nfc/MainActivity.java
@@ -1,22 +1,35 @@
 package com.example.express_nfc;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.PendingIntent;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
 import android.nfc.tech.MifareClassic;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,6 +47,14 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonGetLast;
     private Button buttonGetWithID;
     private Button buttonDelete;
+    private WebView listWebView;
+    private WebView calendarWebView;
+
+
+
+    public final String ip = "http://127.0.0.1:3000/";
+    public final String listIp = "https://google.com";
+    public final String calendarIp = "https://google.com";
 
     private void initViews() {
         labelID = findViewById(R.id.textView);
@@ -43,6 +64,41 @@ public class MainActivity extends AppCompatActivity {
         buttonGetWithID = findViewById(R.id.button2);
         buttonGetLast = findViewById(R.id.button3);
         buttonDelete = findViewById(R.id.button4);
+        calendarWebView = findViewById(R.id.webView);
+        listWebView = findViewById(R.id.webView2);
+        calendarWebView.setWebViewClient(new WebViewClient());
+        listWebView.setWebViewClient(new WebViewClient());
+        listWebView.loadUrl(listIp);
+        calendarWebView.loadUrl(calendarIp);
+    }
+
+    public boolean scannedTag(String tag){
+        boolean returnValue = true;
+        if(tag.equals("00000000")){
+            returnValue = false;
+        }
+        return returnValue;
+    }
+
+    public void alertMessage(String title,String message){
+        AlertDialog.Builder alertDialog =
+                new AlertDialog.Builder(MainActivity.this);
+        alertDialog.setTitle(title);
+        alertDialog.setMessage(message);
+        alertDialog.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+            }
+        });
+        alertDialog.setCancelable(false);
+        alertDialog.show();
+    }
+
+    public void reflashList(String ip){
+        listWebView.loadUrl(ip);
+    }
+
+    public void reflashCalendar(String ip){
+        calendarWebView.loadUrl(ip);
     }
 
     @Override
@@ -57,7 +113,7 @@ public class MainActivity extends AppCompatActivity {
             this.finish();
             return;
         }
-        mPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this,getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        mPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
 
         IntentFilter filter = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
         try {
@@ -68,10 +124,42 @@ public class MainActivity extends AppCompatActivity {
         mIntentFilters = new IntentFilter[]{filter};
         mTechList = new String[][]{new String[]{MifareClassic.class.getName()}}; // 只處理 MifareClassic 的 tag
 
-        buttonGetLast.setOnClickListener(new View.OnClickListener() {
+        buttonPost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String url = "http://192.168.0.120:3000/nfc/list";  // Request a string response
+                if(!scannedTag(idView.getText().toString())){
+                    alertMessage("錯誤訊息","未感測到任何卡片，請感測卡片。");
+                    return;
+                }
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                String url = ip + "schedules";  // Request a string response
+
+                Map<String, String> map = new HashMap<>();
+                map.put("tagid", idView.getText().toString());
+
+                JSONObject postData = new JSONObject(map);
+
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, postData, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject r) {
+                        System.out.println(r);
+                        responseView.setText("Http response about getting a web page " + url + " is:" + Html.fromHtml("<br><br>" + r));
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        responseView.setText("Something went wrong!");
+                        error.printStackTrace();
+                    }
+                });
+                queue.add(jsonObjectRequest);
+            } //onClick(View v)
+        }); // buttonGetLast.setOnClickListener(…)
+
+        buttonGetWithID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = ip + "schedules";  // Request a string response
                 StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                         new Response.Listener<String>() {
                             @Override
@@ -85,7 +173,49 @@ public class MainActivity extends AppCompatActivity {
                         error.printStackTrace();
                     }
                 });// Add the request to the queue
-                Volley.newRequestQueue(this).add(stringRequest);
+                Volley.newRequestQueue(MainActivity.this).add(stringRequest);
+            } //onClick(View v)
+        }); // buttonGetLast.setOnClickListener(…)
+
+        buttonGetLast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = ip + "schedules/last";  // Request a string response
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) { // Result handling
+                                responseView.setText(response);
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {// Error handling
+                        responseView.setText("Something went wrong!");
+                        error.printStackTrace();
+                    }
+                });// Add the request to the queue
+                Volley.newRequestQueue(MainActivity.this).add(stringRequest);
+            } //onClick(View v)
+        }); // buttonGetLast.setOnClickListener(…)
+
+        buttonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = ip + "schedules/last";  // Request a string response
+                StringRequest stringRequest = new StringRequest(Request.Method.DELETE, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) { // Result handling
+                                responseView.setText(response);
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {// Error handling
+                        responseView.setText("Something went wrong!");
+                        error.printStackTrace();
+                    }
+                });// Add the request to the queue
+                Volley.newRequestQueue(MainActivity.this).add(stringRequest);
             } //onClick(View v)
         }); // buttonGetLast.setOnClickListener(…)
     }
